b0ee36ed4ba400a068ea6b88a19ba708dc51827c
Ethan <Ethan.lynn2027@gbwl.org>
2025-12-02 16:12:57 +0000

    added stuff

--- Main.java
+++ Main.java
@@ -16,9 +16,94 @@
       if ( command.equals("e") ) {
          // NEXT BRANCH...
          System.out.println("You reach the Murray hill muse.");
+      
+         command = getCommand("Do you go north or east");
+         if (command.equals("north")){
+            System.out.println("You see Garlic Pizzeria.");
+         }
+         else if (command.equals("east")){
+            System.out.println("You illegaly enter the building and get shot by the doorman! Game Over! Press run to replay.");
+         }
+   
+         command = getCommand("Would you like to go inside?");
+         if (command.equals("yes")){
+      System.out.println("pick a slice");
+         }
+         else if (command.equals ("no")){
+            System.out.println("You get hit by a taxi! Game over! Press run to restart.");
+         }
+      command = getCommand("What slice would you like?");
+         if (command.equals("cheese")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+              
+         if (command.equals("cheese")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+        else if (command.equals("vodka")){
+            System.out.println("Correct! Enjoy the slice!");
+         }
+         else if (command.equals("pasta")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("grandma")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("barbeque chicken")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("peperoni")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("hawian")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+       else if (command.equals("buffalo chicken")){
+            System.out.println("Correct! Enjoy the slice!");
+         }
       }
+      
+      
       else if ( command.equals("n") ) {
          System.out.println("You see Garlic Pizzeria.");
+      
+         command = getCommand("Would you like to go inside?");
+         if (command.equals("yes")){
+      System.out.println("pick a slice");
+         }
+         else if (command.equals ("no")){
+            System.out.println("You get hit by a taxi! Game over! Press run to restart.");
+         }
+      command = getCommand("What slice would you like?");
+         if (command.equals("cheese")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+        else if (command.equals("vodka")){
+            System.out.println("Correct! Enjoy the slice!");
+         }
+         else if (command.equals("pasta")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("grandma")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("barbeque chicken")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("peperoni")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+         else if (command.equals("hawian")){
+            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+         }
+       else if (command.equals("buffalo chicken")){
+            System.out.println("Correct! Enjoy the slice!");
+         }
+         command=getCommand("Would you like to go north, east, or west");
+
+
+
+
       }
       else if (command.equals("s")) {
          System.out.println("GAME OVER! You reached the FDR and are out of Murray hill. Press run to replay.");
@@ -40,7 +125,7 @@
        System.out.println("\n▶︎▶︎▶︎ " + prompt);
        Scanner scan = new Scanner(System.in);
        String command = scan.nextLine().toLowerCase();
-       scan.close();
+      
        return command;
     } // END OF METHOD
  } // END OF CLASS
