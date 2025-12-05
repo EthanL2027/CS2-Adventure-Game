d914f56b6512823cee8791805fbbce9e6851a3a5
Ethan <Ethan.lynn2027@gbwl.org>
2025-12-05 13:53:40 +0000

    Finished game

--- Main.java
+++ Main.java
@@ -1,185 +1,180 @@
- import java.util.Scanner;
- public class Main 
- {
-    public static void main(String []args) 
-    {
-       System.out.println("Starting a new adventure...\n");
-       
-       System.out.println("Your goal is to get to Ethans house, but you must complete steps along the way");
-       String command = ""; // stores the user's choices
+import java.util.Scanner;
 
-       // STEP #1: Describe the current scenario
-       System.out.println("You are at the center of Saint Vartens park in Murray Hill.");
-       //STEP #2: Get user command
-       command = getCommand("Do you go (e), (w), (n), or (s)");
-       // STEP #3: Select a path based on the user's command
-      if ( command.equals("e") ) {
+public class Main {
+   public static void main(String[] args) {
+      System.out.println("Starting a new adventure...\n");
+
+      System.out.println("Your goal is to get Murray Hill citizenship, but you must complete steps along the way");
+      String command = ""; // stores the user's choices
+
+      // STEP #1: Describe the current scenario
+      System.out.println("You are at the center of Saint Vartens park in Murray Hill.");
+      // STEP #2: Get user command
+      command = getCommand("Do you go (e), (w), (n), or (s)");
+      // STEP #3: Select a path based on the user's command
+      if (command.equals("e")) {
          // NEXT BRANCH...
          System.out.println("You reach the Murray hill muse.");
-      
+
          command = getCommand("Do you go north or east");
-         if (command.equals("north")){
+         if (command.equals("north")) {
             System.out.println("You see Garlic Pizzeria.");
             command = getCommand("Would you like to go inside?");
+         } else if (command.equals("east")) {
+            System.out.println(
+                  "You illegaly enter the building and get shot by the doorman! Game Over! Press run to replay.");
          }
-         else if (command.equals("east")){
-            System.out.println("You illegaly enter the building and get shot by the doorman! Game Over! Press run to replay.");
-         }
-   
-         
-         if (command.equals("yes")){
+
+         if (command.equals("yes")) {
             command = getCommand("What slice would you like?");
 
-        if (command.equals("vodka")){
-            System.out.println("Correct! Enjoy the slice!");
-            command = getCommand("Would you like to go north or east");
-         if(command.equals("north")){
-            System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
-            command = getCommand("Is murr sigma or skibidi?");
+            if (command.equals("vodka")) {
+               System.out.println("Correct! Enjoy the slice!");
+               command = getCommand("Would you like to go north or east");
+               if (command.equals("north")) {
+                  System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
+                  command = getCommand("Is murr sigma or skibidi?");
+               }
+               if (command.equals("sigma")) {
+                  System.out.println("Correct! You have been granted Murray Hill citizneship!");
+               } else if (command.equals("skibidi")) {
+                  System.out.println(
+                        "Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
+               }
+               else if (command.equals("east")) {
+                   System.out.println("You walk into the random ahh Murray Hill parking lot and explode! Press run to replay.");
+               }
+            } else if (command.equals("pasta")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } else if (command.equals("grandma")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } else if (command.equals("barbeque chicken")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } else if (command.equals("peperoni")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } else if (command.equals("hawian")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } else if (command.equals("buffalo chicken")) {
+               System.out.println("Correct! Enjoy the slice!");
+               command = getCommand("Would you like to go north or east");
+               if (command.equals("north")) {
+                  System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
+                  command = getCommand("Is murr sigma or skibidi?");
+               }
+               if (command.equals("sigma")) {
+                  System.out.println("Correct! You have been granted Murray Hill citizneship!");
+               } else if (command.equals("skibidi")) {
+                  System.out.println(
+                        "Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
+               }
+               else if (command.equals("east")) {
+                   System.out.println("You walk into the random ahh Murray Hill parking lot and explode! Press run to replay.");
+               }
+            }
+
          }
-         if(command.equals("sigma")){
-            System.out.println("Correct! You have been granted Murray Hill citizneship!");
-         }
-         else if(command.equals("skibidi")){
-            System.out.println("Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
-         }
-         }
-         else if (command.equals("pasta")){
+         if (command.equals("cheese")) {
             System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
          }
-         else if (command.equals("grandma")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         else if (command.equals("barbeque chicken")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         else if (command.equals("peperoni")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         else if (command.equals("hawian")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-       else if (command.equals("buffalo chicken")){
-            System.out.println("Correct! Enjoy the slice!");
-            command = getCommand("Would you like to go north or east");
-         if(command.equals("north")){
-            System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
-            command = getCommand("Is murr sigma or skibidi?");
-         }
-         if(command.equals("sigma")){
-            System.out.println("Correct! You have been granted Murray Hill citizneship!");
-         }
-         else if(command.equals("skibidi")){
-            System.out.println("Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
-         }
-         }
-      
-         }
-         if (command.equals("cheese")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-              
-         
-         
-         else if (command.equals ("no")){
+
+         else if (command.equals("no")) {
             System.out.println("You get hit by a taxi! Game over! Press run to restart.");
          }
       }
-      
-      
-      else if ( command.equals("n") ) {
+
+      else if (command.equals("n")) {
          System.out.println("You see Garlic Pizzeria.");
-      
+
          command = getCommand("Would you like to go inside?");
-         if (command.equals("yes")){
-      
-         command = getCommand("What slice would you like?");
-         if (command.equals("cheese")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         
+         if (command.equals("yes")) {
 
-        else if (command.equals("vodka")){
-            System.out.println("Correct! Enjoy the slice!");
-            command = getCommand("Would you like to go north or east");
-         if(command.equals("north")){
-            System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
-            command = getCommand("Is murr sigma or skibidi?");
-         }
-         if(command.equals("sigma")){
-            System.out.println("Correct! You have been granted Murray Hill citizneship!");
-         }
-         else if(command.equals("skibidi")){
-            System.out.println("Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
-         }
-         }
-         else if (command.equals("pasta")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         else if (command.equals("grandma")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         else if (command.equals("barbeque chicken")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         else if (command.equals("peperoni")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-         else if (command.equals("hawian")){
-            System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
-         }
-       else if (command.equals("buffalo chicken")){
-            System.out.println("Correct! Enjoy the slice!");
-            command = getCommand("Would you like to go north or east");
-          
-          if(command.equals("north")){
-            System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
-            command = getCommand("Is murr sigma or skibidi?");
-         }
-         if(command.equals("sigma")){
-            System.out.println("Correct! You have been granted Murray Hill citizneship!");
-         }
-         else if(command.equals("skibidi")){
-            System.out.println("Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
-         }
-         
-         }
-         
-         
+            command = getCommand("What slice would you like?");
+            if (command.equals("cheese")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            }
 
+            else if (command.equals("vodka")) {
+               System.out.println("Correct! Enjoy the slice!");
+               command = getCommand("Would you like to go north or east");
+               if (command.equals("north")) {
+                  System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
+                  command = getCommand("Is murr sigma or skibidi?");
+               }
+               if (command.equals("sigma")) {
+                  System.out.println("Correct! You have been granted Murray Hill citizneship!");
+               } else if (command.equals("skibidi")) {
+                  System.out.println(
+                        "Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
+               }
+               else if (command.equals("east")) {
+                   System.out.println("You walk into the random ahh Murray Hill parking lot and explode! Press run to replay.");
+               }
+            } 
+            else if (command.equals("pasta")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } 
+            else if (command.equals("grandma")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } 
+            else if (command.equals("barbeque chicken")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } 
+            else if (command.equals("peperoni")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } 
+            else if (command.equals("hawian")) {
+               System.out.println("WRONG ANSWER! Game Over! Press run to replay.");
+            } 
+            else if (command.equals("buffalo chicken")) {
+               System.out.println("Correct! Enjoy the slice!");
+               command = getCommand("Would you like to go north or east");
+
+               if (command.equals("north")) {
+                  System.out.println("You spot Sam leibman outside Stickys (RIP) paying his respect and he asks  ");
+                  command = getCommand("Is murr sigma or skibidi?");
+
+                  if (command.equals("sigma")) {
+                  System.out.println("Correct! You have been granted Murray Hill citizneship!");
+                  } 
+                  else if (command.equals("skibidi")) {
+                     System.out.println("Wrong! Sam hits you with the murr dance off and sends you into the earths core! Press run to replay");
+                  }
+               } 
+                 
+               else if (command.equals("east")) {
+                   System.out.println("You walk into the random ahh Murray Hill parking lot and explode! Press run to replay.");
+               }
+            
+                
+                  
+
+            }
+            
          }
-         
 
-
-         else if (command.equals ("no")){
+         else if (command.equals("no")) {
             System.out.println("You get hit by a taxi! Game over! Press run to restart.");
          }
-      
 
-
-
-      }
-      else if (command.equals("s")) {
+      } else if (command.equals("s")) {
          System.out.println("GAME OVER! You reached the FDR and are out of Murray hill. Press run to replay.");
-      }
-      else if (command.equals("w")) {
+      } else if (command.equals("w")) {
          System.out.println("GAME OVER! You reached the fence and got stuck in the park. Press run to replay");
       }
 
+   } // END OF MAIN METHOD
 
-          
-    } // END OF MAIN METHOD
-    
-    /** Method to prompt & process input for the next command
-      * @param prompt to display to user, asks for specific input
-      * @return user's input command as a lowercase String
+   /**
+    * Method to prompt & process input for the next command
+    * 
+    * @param prompt to display to user, asks for specific input
+    * @return user's input command as a lowercase String
     */
-    public static String getCommand(String prompt) 
-    {
-       System.out.println("\n▶︎▶︎▶︎ " + prompt);
-       Scanner scan = new Scanner(System.in);
-       String command = scan.nextLine().toLowerCase();
-      
-       return command;
-    } // END OF METHOD
- } // END OF CLASS
+   public static String getCommand(String prompt) {
+      System.out.println("\n▶︎▶︎▶︎ " + prompt);
+      Scanner scan = new Scanner(System.in);
+      String command = scan.nextLine().toLowerCase();
+
+      return command;
+   } // END OF METHOD
+} // END OF CLASS
